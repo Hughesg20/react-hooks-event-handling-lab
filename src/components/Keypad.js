29718f189070c8666d9ee052fced2fff2d94767e
Griffin Hughes <Griffinhughes2020@gmail.com>
2022-02-02 17:04:57 -0500

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+
+
+
+function enteringPassword(){
+    console.log('Entering password...')
+}
+
+
+
+function Keypad(){
+    return(
+    <input onChange={enteringPassword}type="password" />
+    )
+}
+
+export default Keypad;
